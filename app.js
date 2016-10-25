937b388399ed3ea19806cfbdd5b20297711af66e
Doug Riblet <smashzen85@gmail.com>
2016-10-25 18:00:52 -0500

    add Procfile

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/make/:input', function(req, res) {
+
+});
+
+
+
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
