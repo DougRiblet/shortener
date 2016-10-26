7ded703e94913b6487639f4132baa093e537dcce
Doug Riblet <smashzen85@gmail.com>
2016-10-26 15:54:17 -0500

    add Validator module to test URL submissions

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 var express = require("express");
 var path    = require("path");
+var validator = require('validator');
 
 var app = express();
 var port = process.env.PORT || 8060;
 
 app.get('/', function (req, res) {
+	console.log("=== reaching home")
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/make/:input', function(req, res) {
+	console.log("=== reaching make3")
+	var original = req.params.input;
+	var code = "0001";
+	var output;
+
+  if (validator.isURL(original)){
+  	output = "https://szurl.herokuapp.com/" + code;
+  } else {
+  	output = "Not a valid URL";
+  }
+
+	res.json({"original":original,"shortened":output});
 
 });
 
