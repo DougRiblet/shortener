d05e1b71172d32401613ce8430f34a9f3078a111
Doug Riblet <smashzen85@gmail.com>
2016-10-28 17:45:38 -0500

    delete some console logs, reword others

--- app.js
+++ app.js
@@ -10,20 +10,17 @@ var port = process.env.PORT || 8060;
 // ENDPOINTS
 
 app.get('/', function (req, res) {
-	console.log("=== reaching home")
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.get('/make/:input', function(req, res) {
-	console.log("=== reaching make")
 	var original = req.params.input;
   if (validator.isURL(original)){
   	var submission = new Link({original:original});
 		submission.save(function (err, data) {
   		if (err) {
-  			console.log("link create error: ", err);
+  			console.log("link submission error: ", err);
   		} else {
-  			console.log("data returned when creating new link: ", data);
   			res.json({
   				"original":data.original,
   				"shortened":"https://szurl.herokuapp.com/go/"+data.shortened
@@ -69,5 +66,5 @@ mongoose.connection.on('disconnected', function(){
 // SERVER
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
+  console.log('SZURL app listening on port ' + port);
 });
